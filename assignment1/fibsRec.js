afcfaff9604a36ce77ed1f394583822fce2812dc
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2024-01-23 07:04:36 +0100

    Fix fibsRec.js

--- assignment1/fibsRec.js
+++ assignment1/fibsRec.js
@@ -1,6 +1,15 @@
-function fibsRec(num, a=0, b=1, arr=[]) {
-    if (num < 1) return arr;
-    return [a].concat(fibsRec(num -1, b, b + a, arr));
+// function fibsRec(num, a=0, b=1, arr=[]) {
+//     if (num < 1) return arr;
+//     return [a].concat(fibsRec(num -1, b, b + a, arr));
+// }
+
+function fibsRec(num) {
+    if (num === 2) return [0, 1]
+
+    let arr = fibsRec(num - 1)
+    arr.push(arr[arr.length-1] + arr[arr.length-2]);
+    return arr;
 }
 
-console.log(fibsRec(8))
+
+console.log(fibsRec(8));
