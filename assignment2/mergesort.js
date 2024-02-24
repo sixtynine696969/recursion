b619be9bd6f89536e915d5dc4efafd3e36706071
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2024-02-24 19:16:39 +0100

    Write mergesort function

---
+++ assignment2/mergesort.js
@@ -0,0 +1,29 @@
+function mergesort(arr) {
+    if (arr.length == 1) return arr;
+
+    let arr1 = mergesort(arr.splice(0, arr.length/2))
+    let arr2 = mergesort(arr);
+    let finalArray = [];
+
+    while (arr1.length && arr2.length) {
+        if (arr1[0] <= arr2[0]) {
+            finalArray.push(arr1.shift())
+        } else if (arr2[0] <= arr1[0]){
+            finalArray.push(arr2.shift())
+        }
+    }
+
+    (arr1.length) ? finalArray.push(...arr1) : finalArray.push(...arr2);
+
+    // if (!arr1.length) {
+    //     finalArray.push(...arr2)
+    // } else if (!arr2.length) {
+    //     finalArray.push(...arr1);
+    // }
+
+    return finalArray;
+}
+
+console.log(mergesort([3, 2, 1, 13, 8, 5, 0, 1]));
+
+console.log(mergesort([105, 79, 100, 110]));
